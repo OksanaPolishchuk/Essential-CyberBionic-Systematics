4a7579af60029b05d6ddd86df40dac9b3bec4bbe
Oksana <opolishchuk89@outlook.com>
2020-02-10 20:42:51 +0200

    PolygonApp with new additions

---
+++ Essential/PolygonApp/PolygonApp/Model/FigureType.cs
@@ -0,0 +1,12 @@
+﻿namespace PolygonApp.Model
+{
+    public enum FigureType
+    {
+        None = 0,
+        Point = 1,
+        Line = 2,
+        Triangle = 3,
+        Square = 4,
+        Polygon = 5
+    }
+}
