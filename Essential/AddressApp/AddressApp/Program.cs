23c698e559fb6a5e26fa9de7e5e470b78f2c69e7
Oksana <opolishchuk89@outlook.com>
2020-02-15 15:49:41 +0200

    PolygonApp with new additionsstatus

--- Essential/AddressApp/AddressApp/Program.cs
+++ Essential/AddressApp/AddressApp/Program.cs
@@ -1,10 +1,20 @@
-﻿
+﻿using System;
+using AddressApp.Model;
+
 namespace AddressApp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Address address = new Address();
+
+            address.Index = " 01001";
+            address.Apartment = "23";
+
+            Console.WriteLine( "Index: " + address.Index + "\n" + "Country: " + address.Country + "\n" + "City: " + address.City + "\n" + "Street: " + address.Street + "\n" + "House: " + address.House + "\n" + "Apartment: " + address.Apartment);
+
+            Console.ReadKey();
         }
     }
 }
